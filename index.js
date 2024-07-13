7829d3cee8a333f70877f223bc1c078d3fc93b7c
Willigers <ronowilligers@gmail.com>
2024-07-13 20:00:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Rono",
+  streetAddress: "30200 Kitale Nairobi",
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  return {
+    ...employee,
+    [key]: value,
+  };
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  const updatedEmployeeDetails = {
+    ...employee,
+  };
+  delete updatedEmployeeDetails[key];
+  return updatedEmployeeDetails;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+};
